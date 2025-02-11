b29f160e98f3eafee9148473ea8317f17f74d236
Nemtorson <128622515+Nemtorson@users.noreply.github.com>
2025-02-11 17:53:28 +0100

    Initial Commit CardView isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: false)
         }
+        .foregroundColor(.blue)
+        .imageScale(.small)
         .padding()
     }
-}
+} //XD
 
 #Preview {
     ContentView()
 }
+
+
+struct CardView: View {
+    @State var isFaceUp = false //@State creates a pointer
+    // var isFaceUp: Bool = false
+    var body: some View {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base
+                    .foregroundColor(.white)
+                base
+                    .strokeBorder(lineWidth: 4)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
